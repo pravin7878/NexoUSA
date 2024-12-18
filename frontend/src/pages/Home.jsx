e05644c6d34d217455214c32ebc175dd2c6ceddb
Pravin Kumar <pravinmimrot648@gmail.com>
2024-12-18 23:29:22 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,8 @@ import Description from "../components/home/Description";
 import FabricationComponent from "../components/home/FabricationComponent";
 import FastenerComponent from "../components/home/FastenerComponent";
 import FastnersLinks from "../components/home/FastnersLinks";
-// import { Slider } from "../components/home/Slider";
-import SwiperComponent from "../components/home/SwiperComponent";
+import { Slider } from "../components/home/Slider";
+// import SwiperComponent from "../components/home/SwiperComponent";
 import WelcomeSection from "../components/home/WelcomeSection";
 
 // Slider Component 
