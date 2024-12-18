bdba7ee9a697b560a3697843ddbc61c3856a0673
Pravin Kumar <pravinmimrot648@gmail.com>
2024-12-19 01:16:20 +0530

    push

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,10 @@ import Description from "../components/home/Description";
 import FabricationComponent from "../components/home/FabricationComponent";
 import FastenerComponent from "../components/home/FastenerComponent";
 import FastnersLinks from "../components/home/FastnersLinks";
-import { Slider } from "../components/home/Slider";
 // import SwiperComponent from "../components/home/SwiperComponent";
 import WelcomeSection from "../components/home/WelcomeSection";
+import {Slider} from "../utils/Slider"
+
 
 // Slider Component 
 
@@ -17,7 +18,7 @@ const Home = () => {
     <>
       {/* <h1 className="p-10 text-center">This is Home Page </h1> */}
       <div>
-        <Slider />
+        <Slider/>
         {/* <SwiperComponent /> */}
         <WelcomeSection />
         <FastenerComponent />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        roboto: ['Roboto', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        bebas: ['Bebas Neue', 'cursive'],
-        noto: ['Noto Sans', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        bebas: ["Bebas Neue", "cursive"],
+        noto: ["Noto Sans", "sans-serif"],
+      },
+      keyframes: {
+        scaleUp: {
+          "0%": {
+            transform: "scale(0.5)",
+          },
+          "100%": { transform: "scale(1)" },
+        },
+        slideIn: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        slideOut: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        scaleUp: "scaleUp 1s ease-in-out",
+        slideInLeft: "slideIn 1.2s ease-in-out",
+        slideInRight: "slideOut 1.2s ease-in-out",
       },
       // colors: {
       //   themeCol: "#fb8500"
@@ -19,4 +37,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
